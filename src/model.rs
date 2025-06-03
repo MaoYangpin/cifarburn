5219fa247d55fc2d464b5213daaa132551d09280
MaoYangpin <mao.yangpin@gmail.com>
2025-06-03 11:10:13 +0800

    refactor(model): simplify Cifar10Model to basic MLP architecture
    Removed all convolutional layers, batch normalization, pooling and
    dropout
    components. Changed model to simple multi-layer perceptron with:
    - Input layer (3*32*32 -> hidden_size)
    - ReLU activation
    - Output layer (hidden_size -> num_classes)
    Default hidden_size reduced from 512 to 128.

--- src/model.rs
+++ src/model.rs
@@ -1,21 +1,10 @@
 use burn::{
-    nn::{
-        BatchNorm, BatchNormConfig, Dropout, DropoutConfig, Linear, LinearConfig, Relu,
-        conv::{Conv2d, Conv2dConfig},
-        pool::{MaxPool2d, MaxPool2dConfig},
-    },
+    nn::{Linear, LinearConfig, Relu},
     prelude::*,
 };
 
 #[derive(Module, Debug)]
 pub struct Cifar10Model<B: Backend> {
-    conv1: Conv2d<B>,
-    bn1: BatchNorm<B, 2>,
-    pool1: MaxPool2d,
-    conv2: Conv2d<B>,
-    bn2: BatchNorm<B, 2>,
-    pool2: MaxPool2d,
-    dropout: Dropout,
     linear1: Linear<B>,
     linear2: Linear<B>,
     activation: Relu,
@@ -25,56 +14,26 @@ pub struct Cifar10Model<B: Backend> {
 pub struct Cifar10ModelConfig {
     #[config(default = "10")]
     num_classes: usize,
-    #[config(default = "512")]
+    #[config(default = "128")]
     hidden_size: usize,
-    #[config(default = "0.5")]
-    dropout: f64,
 }
 
 impl Cifar10ModelConfig {
     pub fn init<B: Backend>(&self, device: &B::Device) -> Cifar10Model<B> {
         Cifar10Model {
-            conv1: Conv2dConfig::new([3, 32], [5, 5]).init(device),
-            bn1: BatchNormConfig::new(32).init(device),
-            pool1: MaxPool2dConfig::new([2, 2]).init(),
-            conv2: Conv2dConfig::new([32, 64], [5, 5]).init(device),
-            bn2: BatchNormConfig::new(64).init(device),
-            pool2: MaxPool2dConfig::new([2, 2]).init(),
-            dropout: DropoutConfig::new(self.dropout).init(),
-            activation: Relu::new(),
-            linear1: LinearConfig::new(64 * 22 * 22, self.hidden_size).init(device),
+            linear1: LinearConfig::new(3 * 32 * 32, self.hidden_size).init(device),
             linear2: LinearConfig::new(self.hidden_size, self.num_classes).init(device),
+            activation: Relu::new(),
         }
     }
 }
 
 impl<B: Backend> Cifar10Model<B> {
     pub fn forward(&self, images: Tensor<B, 4>) -> Tensor<B, 2> {
-        // Keep this dbg! to confirm initial input is still 32x32
-        dbg!(&images.dims(), "Input to model.forward");
-
-        let [batch_size, _channels, _height, _width] = images.dims();
-
-        let x = self.conv1.forward(images);
-        let x = self.bn1.forward(x);
-        let x = self.activation.forward(x);
-        let x = self.pool1.forward(x);
-
-        let x = self.conv2.forward(x);
-        let x = self.bn2.forward(x);
-        let x = self.activation.forward(x);
-        let x = self.pool2.forward(x);
-        let x = self.dropout.forward(x);
-
-        // ADD THIS dbg! TO SEE SHAPE JUST BEFORE RESHAPE
-        dbg!(&x.dims(), "Shape before final reshape");
-
-        let x = x.reshape([batch_size, 64 * 22 * 22]); // This is the line that panics
-
+        let [batch_size, ..] = images.dims();
+        let x = images.reshape([batch_size, 3 * 32 * 32]);
         let x = self.linear1.forward(x);
         let x = self.activation.forward(x);
-        let x = self.dropout.forward(x);
-
         self.linear2.forward(x)
     }
 }
